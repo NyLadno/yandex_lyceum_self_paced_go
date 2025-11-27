11bcf0bc700bc7275f2f24b2c41aa6181662f320
Lunavv-cloud <nyladno14@gmail.com>
2025-11-27 21:28:24 +0300

    Пример функции форматирования от наибольшего к меньшему

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	nums := []int8{12, 78, 124, 56, 80}
+
+	slices.SortFunc(nums, func(a, b int8) int{
+		switch{
+		case a < b:
+			return 1
+		case a > b:
+			return -1
+		default:
+			return 0
+		}
+})
+
+	fmt.Println(nums)
 }
