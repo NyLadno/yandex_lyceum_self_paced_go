a421fc22052ad3bae620eda9a728994d05c45cba
Lunavv-cloud <nyladno14@gmail.com>
2025-11-29 08:53:13 +0300

    Задача: Абвгд. Суть задачи отсартировать учеников в алфовитном порядке

--- main.go
+++ main.go
@@ -5,19 +5,11 @@ import (
 	"slices"
 )
 
+func SortNames(names []string) []string{
+	slices.Sort(names)
+	return names
+}
+
 func main() {
-	nums := []int8{12, 78, 124, 56, 80}
-
-	slices.SortFunc(nums, func(a, b int8) int{
-		switch{
-		case a < b:
-			return 1
-		case a > b:
-			return -1
-		default:
-			return 0
-		}
-})
-
-	fmt.Println(nums)
+	fmt.Println(SortNames([]string{"Аксинья", "Варвара", "Арина", "Есения"}))
 }
