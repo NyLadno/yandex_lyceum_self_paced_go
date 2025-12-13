d7662f260eeddbd40862959963861fb55ccf06c4
Lunavv-cloud <nyladno14@gmail.com>
2025-12-13 13:37:11 +0300

    Задача: Ограничение цены; Суть задачи ограничить цену от в соотсвествии с условию: цена меньше минимальной вывести минимальную, выше масимальной вывести масимальную

--- main.go
+++ main.go
@@ -1,54 +1,15 @@
 package main
 
-import "fmt"
+const minPrice = 99.0
+const maxPrice = 20000
 
-type WeatherCondition int
-
-const (
-    Clear WeatherCondition = iota
-    Rain
-    HeavyRain
-    Snow
-)
-
-type WeatherData struct {
-    Condition WeatherCondition
-    WindSpeed int
-}
-
-const basePrise = 1
-
-func GetWeatherMultiplier(weather WeatherData) float64{
+func ApplyPriceLimits(price float64) float64 {
 	switch{
-	case weather.Condition == Clear && weather.WindSpeed > 15:
-		return basePrise + 0.1
-	case weather.Condition == HeavyRain && weather.WindSpeed > 15:
-		return basePrise + 0.3
-	case weather.Condition == Rain && weather.WindSpeed > 15:
-		return  basePrise + 0.225
-	case weather.Condition == Snow && weather.WindSpeed > 15:
-		return  basePrise + 0.25	
-	case weather.Condition == HeavyRain:
-		return basePrise + 0.2
-	case weather.Condition == Rain:
-		return  basePrise + 0.125
-	case weather.Condition == Snow:
-		return  basePrise + 0.15
+	case price < minPrice:
+		return minPrice
+	case price > maxPrice:
+		return maxPrice
 	default:
-		return basePrise
-	}	
-}
-
-func main() {
-	fmt.Println(GetWeatherMultiplier(WeatherData{HeavyRain, 10}))
-	fmt.Println(GetWeatherMultiplier(WeatherData{HeavyRain, 20}))
-
-	fmt.Println(GetWeatherMultiplier(WeatherData{Rain, 10}))
-	fmt.Println(GetWeatherMultiplier(WeatherData{Rain, 20}))
-
-	fmt.Println(GetWeatherMultiplier(WeatherData{Clear, 10}))
-	fmt.Println(GetWeatherMultiplier(WeatherData{Clear, 20}))
-
-	fmt.Println(GetWeatherMultiplier(WeatherData{Snow, 10}))
-	fmt.Println(GetWeatherMultiplier(WeatherData{Snow, 20}))
+		return price
+	}
 }
