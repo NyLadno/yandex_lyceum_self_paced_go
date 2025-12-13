30d1f56e2b33d90ca8956e16ccc117c137378332
Lunavv-cloud <nyladno14@gmail.com>
2025-12-13 11:58:38 +0300

    Задача: Загруженность дорог; Высчитывание коэфицента цены на поездку в такси в зависимости от погоды.

--- main.go
+++ main.go
@@ -2,25 +2,53 @@ package main
 
 import "fmt"
 
-const pricePerKm = 10.0
-const pricePerMinute = 2.0
+type WeatherCondition int
 
-type TripParameters struct {
-	Distance float64
-	Duration float64
+const (
+    Clear WeatherCondition = iota
+    Rain
+    HeavyRain
+    Snow
+)
+
+type WeatherData struct {
+    Condition WeatherCondition
+    WindSpeed int
 }
 
+const basePrise = 1
 
-func  CalculateBasePrice (t TripParameters) float64 {
-	return t.Distance*pricePerKm + t.Duration*pricePerMinute
+func GetWeatherMultiplier(weather WeatherData) float64{
+	switch{
+	case weather.Condition == Clear && weather.WindSpeed > 15:
+		return basePrise + 0.1
+	case weather.Condition == HeavyRain && weather.WindSpeed > 15:
+		return basePrise + 0.3
+	case weather.Condition == Rain && weather.WindSpeed > 15:
+		return  basePrise + 0.225
+	case weather.Condition == Snow && weather.WindSpeed > 15:
+		return  basePrise + 0.25	
+	case weather.Condition == HeavyRain:
+		return basePrise + 0.2
+	case weather.Condition == Rain:
+		return  basePrise + 0.125
+	case weather.Condition == Snow:
+		return  basePrise + 0.15
+	default:
+		return basePrise
+	}	
 }
 
 func main() {
-	trip1 := TripParameters{Distance: 8.5, Duration: 20}
+	fmt.Println(GetWeatherMultiplier(WeatherData{HeavyRain, 10}))
+	fmt.Println(GetWeatherMultiplier(WeatherData{HeavyRain, 20}))
 
-	fmt.Println(CalculateBasePrice(trip1))
+	fmt.Println(GetWeatherMultiplier(WeatherData{Rain, 10}))
+	fmt.Println(GetWeatherMultiplier(WeatherData{Rain, 20}))
 
-	trip2 := TripParameters{Distance: 15.0, Duration: 22.3}
+	fmt.Println(GetWeatherMultiplier(WeatherData{Clear, 10}))
+	fmt.Println(GetWeatherMultiplier(WeatherData{Clear, 20}))
 
-	fmt.Println(CalculateBasePrice(trip2))
+	fmt.Println(GetWeatherMultiplier(WeatherData{Snow, 10}))
+	fmt.Println(GetWeatherMultiplier(WeatherData{Snow, 20}))
 }
